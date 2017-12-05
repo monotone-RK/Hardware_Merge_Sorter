af0d6d36528368f3258df2315199be60cfb3f9e1
monotone-RK <kobaya4121@gmail.com>
2017-12-06 06:07:57 +0900

    a test bench for the merge tree has been implemented

--- test/tb_hms.v
+++ test/tb_hms.v
@@ -240,89 +240,166 @@
 // endmodule
 
 
-module tb_MERGE_NODE();
+// module tb_MERGE_NODE();
+//   reg CLK; initial begin CLK=0; forever #50 CLK=~CLK; end
+//   reg RST; initial begin RST=1; #400 RST=0; end
+  
+//   wire [(`DATW<<(`E_LOG-1))-1:0] din_a;
+//   wire                           dinen_a;
+//   wire                           full_a;
+  
+//   wire [(`DATW<<(`E_LOG-1))-1:0] din_b;
+//   wire                           dinen_b;
+//   wire                           full_b;
+  
+//   wire [(`DATW<<`E_LOG)-1:0]     merge_node_dot;
+//   wire                           merge_node_doten;
+  
+//   wire [(`DATW<<`E_LOG)-1:0]     fifo4odd_din;
+//   wire [(`DATW<<`E_LOG)-1:0]     fifo4even_din;
+  
+//   wire [(`DATW<<`E_LOG)-1:0]     check_records;
+
+//   reg                            stall_a;
+//   reg                            stall_b;
+     
+//   reg                            flip_selector_a;
+//   reg                            flip_selector_b;
+
+//   always @(posedge CLK) stall_a <= full_a;
+//   always @(posedge CLK) stall_b <= full_b;
+
+//   always @(posedge CLK) begin
+//     if      (RST)     flip_selector_a <= 0;
+//     else if (dinen_a) flip_selector_a <= ~flip_selector_a;
+//   end
+//   always @(posedge CLK) begin
+//     if      (RST)     flip_selector_b <= 0;
+//     else if (dinen_b) flip_selector_b <= ~flip_selector_b;
+//   end
+
+//   genvar i;
+//   generate
+//     for (i=0; i<(1<<`E_LOG); i=i+1) begin: loop
+//       wire [`KEYW-1:0] init_keyodd  = 2 * i + 1;
+//       wire [`KEYW-1:0] init_keyeven = 2 * i + 2;
+//       wire [`KEYW-1:0] chk_rslt_key = i + 1;
+//       reg  [`DATW-1:0] fifo4odd_initrecord;
+//       reg  [`DATW-1:0] fifo4even_initrecord;
+//       reg  [`DATW-1:0] chk_rslt;
+//       always @(posedge CLK) begin
+//         if      (RST)                        fifo4odd_initrecord <= {{(`DATW-`KEYW){1'b1}}, init_keyodd};
+//         else if (dinen_a && flip_selector_a) fifo4odd_initrecord <= fifo4odd_initrecord + (1<<(`E_LOG+1));
+//       end
+//       always @(posedge CLK) begin
+//         if      (RST)                        fifo4even_initrecord <= {{(`DATW-`KEYW){1'b1}}, init_keyeven};
+//         else if (dinen_b && flip_selector_b) fifo4even_initrecord <= fifo4even_initrecord + (1<<(`E_LOG+1));
+//       end
+//       always @(posedge CLK) begin
+//         if      (RST)              chk_rslt <= {{(`DATW-`KEYW){1'b1}}, chk_rslt_key};
+//         else if (merge_node_doten) chk_rslt <= chk_rslt + (1<<`E_LOG);
+//       end
+//       assign fifo4odd_din[`DATW*(i+1)-1:`DATW*i]  = fifo4odd_initrecord;
+//       assign fifo4even_din[`DATW*(i+1)-1:`DATW*i] = fifo4even_initrecord;
+//       assign check_records[`DATW*(i+1)-1:`DATW*i] = chk_rslt;
+//     end
+//   endgenerate
+
+//   assign din_a = (flip_selector_a) ? fifo4odd_din[(`DATW<<`E_LOG)-1:(`DATW<<(`E_LOG-1))] : fifo4odd_din[(`DATW<<(`E_LOG-1))-1:0];
+//   assign din_b = (flip_selector_b) ? fifo4even_din[(`DATW<<`E_LOG)-1:(`DATW<<(`E_LOG-1))] : fifo4even_din[(`DATW<<(`E_LOG-1))-1:0];
+
+//   assign dinen_a = ~stall_a;
+//   assign dinen_b = ~stall_b;
+
+//   MERGE_NODE #(`E_LOG, `DATW, `KEYW)
+//   merge_node(CLK, RST, 1'b0, din_a, dinen_a, din_b, dinen_b, 
+//              full_a, full_b, merge_node_dot, merge_node_doten);
+
+//   always @(posedge CLK) begin
+//     if (merge_node_doten) begin
+//       case (`E_LOG)
+//         1: $write("%d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1]);
+//         2: $write("%d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3]);
+//         3: $write("%d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7]);
+//         4: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_node_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_node_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_node_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_node_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_node_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_node_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_node_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_node_dot[(`KEYW+`DATW*15)-1:`DATW*15]);
+//         5: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_node_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_node_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_node_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_node_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_node_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_node_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_node_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_node_dot[(`KEYW+`DATW*15)-1:`DATW*15], merge_node_dot[(`KEYW+`DATW*16)-1:`DATW*16], merge_node_dot[(`KEYW+`DATW*17)-1:`DATW*17], merge_node_dot[(`KEYW+`DATW*18)-1:`DATW*18], merge_node_dot[(`KEYW+`DATW*19)-1:`DATW*19], merge_node_dot[(`KEYW+`DATW*20)-1:`DATW*20], merge_node_dot[(`KEYW+`DATW*21)-1:`DATW*21], merge_node_dot[(`KEYW+`DATW*22)-1:`DATW*22], merge_node_dot[(`KEYW+`DATW*23)-1:`DATW*23], merge_node_dot[(`KEYW+`DATW*24)-1:`DATW*24], merge_node_dot[(`KEYW+`DATW*25)-1:`DATW*25], merge_node_dot[(`KEYW+`DATW*26)-1:`DATW*26], merge_node_dot[(`KEYW+`DATW*27)-1:`DATW*27], merge_node_dot[(`KEYW+`DATW*28)-1:`DATW*28], merge_node_dot[(`KEYW+`DATW*29)-1:`DATW*29], merge_node_dot[(`KEYW+`DATW*30)-1:`DATW*30], merge_node_dot[(`KEYW+`DATW*31)-1:`DATW*31]);
+//       endcase
+//       $write("\n");
+//       $fflush();
+//     end
+//   end
+
+//   // error checker
+//   always @(posedge CLK) begin
+//     if (merge_node_doten) begin
+//       if (merge_node_dot != check_records) begin
+//         $write("\nError!\n"); 
+//         $finish();
+//       end
+//     end
+//   end
+  
+//   // simulation finish condition
+//   reg [31:0] cycle;
+//   always @(posedge CLK) begin
+//     if (RST) begin
+//       cycle <= 0;
+//     end else begin
+//       cycle <= cycle + 1;
+//       if (cycle >= 100) $finish();
+//     end
+//   end
+  
+// endmodule
+
+
+module tb_MERGE_TREE();
   reg CLK; initial begin CLK=0; forever #50 CLK=~CLK; end
   reg RST; initial begin RST=1; #400 RST=0; end
   
-  wire [(`DATW<<(`E_LOG-1))-1:0] din_a;
-  wire                           dinen_a;
-  wire                           full_a;
-  
-  wire [(`DATW<<(`E_LOG-1))-1:0] din_b;
-  wire                           dinen_b;
-  wire                           full_b;
-  
-  wire [(`DATW<<`E_LOG)-1:0]     merge_node_dot;
-  wire                           merge_node_doten;
-  
-  wire [(`DATW<<`E_LOG)-1:0]     fifo4odd_din;
-  wire [(`DATW<<`E_LOG)-1:0]     fifo4even_din;
-  
-  wire [(`DATW<<`E_LOG)-1:0]     check_records;
+  wire [(`DATW<<`E_LOG)-1:0] merge_tree_din;
+  wire [(1<<`E_LOG)-1:0]     merge_tree_dinen;
+  wire [(1<<`E_LOG)-1:0]     merge_tree_ful;
+  wire [(`DATW<<`E_LOG)-1:0] merge_tree_dot;
+  wire                       merge_tree_doten;
 
-  reg                            stall_a;
-  reg                            stall_b;
-     
-  reg                            flip_selector_a;
-  reg                            flip_selector_b;
+  wire [(`DATW<<`E_LOG)-1:0] check_records;
 
-  always @(posedge CLK) stall_a <= full_a;
-  always @(posedge CLK) stall_b <= full_b;
-
-  always @(posedge CLK) begin
-    if      (RST)     flip_selector_a <= 0;
-    else if (dinen_a) flip_selector_a <= ~flip_selector_a;
-  end
-  always @(posedge CLK) begin
-    if      (RST)     flip_selector_b <= 0;
-    else if (dinen_b) flip_selector_b <= ~flip_selector_b;
-  end
-
+  assign merge_tree_dinen = ~merge_tree_ful;
+  
   genvar i;
   generate
     for (i=0; i<(1<<`E_LOG); i=i+1) begin: loop
-      wire [`KEYW-1:0] init_keyodd  = 2 * i + 1;
-      wire [`KEYW-1:0] init_keyeven = 2 * i + 2;
+      wire [`KEYW-1:0] init_key     = (1<<`E_LOG) - i;
       wire [`KEYW-1:0] chk_rslt_key = i + 1;
-      reg  [`DATW-1:0] fifo4odd_initrecord;
-      reg  [`DATW-1:0] fifo4even_initrecord;
+      reg  [`DATW-1:0] init_record;
       reg  [`DATW-1:0] chk_rslt;
       always @(posedge CLK) begin
-        if      (RST)                        fifo4odd_initrecord <= {{(`DATW-`KEYW){1'b1}}, init_keyodd};
-        else if (dinen_a && flip_selector_a) fifo4odd_initrecord <= fifo4odd_initrecord + (1<<(`E_LOG+1));
-      end
-      always @(posedge CLK) begin
-        if      (RST)                        fifo4even_initrecord <= {{(`DATW-`KEYW){1'b1}}, init_keyeven};
-        else if (dinen_b && flip_selector_b) fifo4even_initrecord <= fifo4even_initrecord + (1<<(`E_LOG+1));
+        if      (RST)                 init_record <= {{(`DATW-`KEYW){1'b1}}, init_key};
+        else if (merge_tree_dinen[i]) init_record <= init_record + (1<<`E_LOG);
       end
       always @(posedge CLK) begin
         if      (RST)              chk_rslt <= {{(`DATW-`KEYW){1'b1}}, chk_rslt_key};
-        else if (merge_node_doten) chk_rslt <= chk_rslt + (1<<`E_LOG);
+        else if (merge_tree_doten) chk_rslt <= chk_rslt + (1<<`E_LOG);
       end
-      assign fifo4odd_din[`DATW*(i+1)-1:`DATW*i]  = fifo4odd_initrecord;
-      assign fifo4even_din[`DATW*(i+1)-1:`DATW*i] = fifo4even_initrecord;
-      assign check_records[`DATW*(i+1)-1:`DATW*i] = chk_rslt;
+      assign merge_tree_din[`DATW*(i+1)-1:`DATW*i] = init_record;
+      assign check_records[`DATW*(i+1)-1:`DATW*i]  = chk_rslt;
     end
   endgenerate
 
-  assign din_a = (flip_selector_a) ? fifo4odd_din[(`DATW<<`E_LOG)-1:(`DATW<<(`E_LOG-1))] : fifo4odd_din[(`DATW<<(`E_LOG-1))-1:0];
-  assign din_b = (flip_selector_b) ? fifo4even_din[(`DATW<<`E_LOG)-1:(`DATW<<(`E_LOG-1))] : fifo4even_din[(`DATW<<(`E_LOG-1))-1:0];
+  MERGE_TREE #(`E_LOG, `DATW, `KEYW)
+  merge_tree(CLK, RST, 1'b0, merge_tree_din, merge_tree_dinen, 
+             merge_tree_ful, merge_tree_dot, merge_tree_doten);
 
-  assign dinen_a = ~stall_a;
-  assign dinen_b = ~stall_b;
-
-  MERGE_NODE #(`E_LOG, `DATW, `KEYW)
-  merge_node(CLK, RST, 1'b0, din_a, dinen_a, din_b, dinen_b, 
-             full_a, full_b, merge_node_dot, merge_node_doten);
-
+  // show result
   always @(posedge CLK) begin
-    if (merge_node_doten) begin
+    if (merge_tree_doten) begin
       case (`E_LOG)
-        1: $write("%d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1]);
-        2: $write("%d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3]);
-        3: $write("%d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7]);
-        4: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_node_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_node_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_node_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_node_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_node_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_node_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_node_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_node_dot[(`KEYW+`DATW*15)-1:`DATW*15]);
-        5: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_node_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_node_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_node_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_node_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_node_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_node_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_node_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_node_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_node_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_node_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_node_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_node_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_node_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_node_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_node_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_node_dot[(`KEYW+`DATW*15)-1:`DATW*15], merge_node_dot[(`KEYW+`DATW*16)-1:`DATW*16], merge_node_dot[(`KEYW+`DATW*17)-1:`DATW*17], merge_node_dot[(`KEYW+`DATW*18)-1:`DATW*18], merge_node_dot[(`KEYW+`DATW*19)-1:`DATW*19], merge_node_dot[(`KEYW+`DATW*20)-1:`DATW*20], merge_node_dot[(`KEYW+`DATW*21)-1:`DATW*21], merge_node_dot[(`KEYW+`DATW*22)-1:`DATW*22], merge_node_dot[(`KEYW+`DATW*23)-1:`DATW*23], merge_node_dot[(`KEYW+`DATW*24)-1:`DATW*24], merge_node_dot[(`KEYW+`DATW*25)-1:`DATW*25], merge_node_dot[(`KEYW+`DATW*26)-1:`DATW*26], merge_node_dot[(`KEYW+`DATW*27)-1:`DATW*27], merge_node_dot[(`KEYW+`DATW*28)-1:`DATW*28], merge_node_dot[(`KEYW+`DATW*29)-1:`DATW*29], merge_node_dot[(`KEYW+`DATW*30)-1:`DATW*30], merge_node_dot[(`KEYW+`DATW*31)-1:`DATW*31]);
+        1: $write("%d %d ", merge_tree_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_tree_dot[(`KEYW+`DATW*1)-1:`DATW*1]);
+        2: $write("%d %d %d %d ", merge_tree_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_tree_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_tree_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_tree_dot[(`KEYW+`DATW*3)-1:`DATW*3]);
+        3: $write("%d %d %d %d %d %d %d %d ", merge_tree_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_tree_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_tree_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_tree_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_tree_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_tree_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_tree_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_tree_dot[(`KEYW+`DATW*7)-1:`DATW*7]);
+        4: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_tree_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_tree_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_tree_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_tree_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_tree_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_tree_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_tree_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_tree_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_tree_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_tree_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_tree_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_tree_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_tree_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_tree_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_tree_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_tree_dot[(`KEYW+`DATW*15)-1:`DATW*15]);
+        5: $write("%d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d %d ", merge_tree_dot[(`KEYW+`DATW*0)-1:`DATW*0], merge_tree_dot[(`KEYW+`DATW*1)-1:`DATW*1], merge_tree_dot[(`KEYW+`DATW*2)-1:`DATW*2], merge_tree_dot[(`KEYW+`DATW*3)-1:`DATW*3], merge_tree_dot[(`KEYW+`DATW*4)-1:`DATW*4], merge_tree_dot[(`KEYW+`DATW*5)-1:`DATW*5], merge_tree_dot[(`KEYW+`DATW*6)-1:`DATW*6], merge_tree_dot[(`KEYW+`DATW*7)-1:`DATW*7], merge_tree_dot[(`KEYW+`DATW*8)-1:`DATW*8], merge_tree_dot[(`KEYW+`DATW*9)-1:`DATW*9], merge_tree_dot[(`KEYW+`DATW*10)-1:`DATW*10], merge_tree_dot[(`KEYW+`DATW*11)-1:`DATW*11], merge_tree_dot[(`KEYW+`DATW*12)-1:`DATW*12], merge_tree_dot[(`KEYW+`DATW*13)-1:`DATW*13], merge_tree_dot[(`KEYW+`DATW*14)-1:`DATW*14], merge_tree_dot[(`KEYW+`DATW*15)-1:`DATW*15], merge_tree_dot[(`KEYW+`DATW*16)-1:`DATW*16], merge_tree_dot[(`KEYW+`DATW*17)-1:`DATW*17], merge_tree_dot[(`KEYW+`DATW*18)-1:`DATW*18], merge_tree_dot[(`KEYW+`DATW*19)-1:`DATW*19], merge_tree_dot[(`KEYW+`DATW*20)-1:`DATW*20], merge_tree_dot[(`KEYW+`DATW*21)-1:`DATW*21], merge_tree_dot[(`KEYW+`DATW*22)-1:`DATW*22], merge_tree_dot[(`KEYW+`DATW*23)-1:`DATW*23], merge_tree_dot[(`KEYW+`DATW*24)-1:`DATW*24], merge_tree_dot[(`KEYW+`DATW*25)-1:`DATW*25], merge_tree_dot[(`KEYW+`DATW*26)-1:`DATW*26], merge_tree_dot[(`KEYW+`DATW*27)-1:`DATW*27], merge_tree_dot[(`KEYW+`DATW*28)-1:`DATW*28], merge_tree_dot[(`KEYW+`DATW*29)-1:`DATW*29], merge_tree_dot[(`KEYW+`DATW*30)-1:`DATW*30], merge_tree_dot[(`KEYW+`DATW*31)-1:`DATW*31]);
       endcase
       $write("\n");
       $fflush();
@@ -331,8 +408,8 @@ module tb_MERGE_NODE();
 
   // error checker
   always @(posedge CLK) begin
-    if (merge_node_doten) begin
-      if (merge_node_dot != check_records) begin
+    if (merge_tree_doten) begin
+      if (merge_tree_dot != check_records) begin
         $write("\nError!\n"); 
         $finish();
       end
@@ -346,10 +423,10 @@ module tb_MERGE_NODE();
       cycle <= 0;
     end else begin
       cycle <= cycle + 1;
-      if (cycle >= 100) $finish();
+      if (cycle >= 200) $finish();
     end
   end
-  
+
 endmodule
 
 `default_nettype wire
